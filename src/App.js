aa8f99985d1127414579d361f8f0e203331df15c
amirhallaji <a.hallaji.b@gmail.com>
2021-06-04 15:45:56 +0430

    basic changes to the app.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
+import antd from 'antd';
 import './App.css';
+import { Component } from 'react';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+
 }
+
+
 
 export default App;
