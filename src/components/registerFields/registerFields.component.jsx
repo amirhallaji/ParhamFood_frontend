b4f98e62c9174e54a3e75ba674c218d4cd499183
amirhallaji <a.hallaji.b@gmail.com>
2021-06-05 12:18:18 +0430

    registerPage component added.

---
+++ src/components/registerFields/registerFields.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './registerFields.styles.css';
+import { Input } from 'antd';
+
+export const RegisterFields = () => {
+      return  (
+            <Input type="text">نام و نام خانوادگی</Input>
+      )
+}
---
+++ src/components/registerPageContainer/registerPageContainer.component.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './registerPageContainer.styles.css';
+
+export const RegisterPageContainer = () => {
+      return (
+            <div></div>
+      )
+}
