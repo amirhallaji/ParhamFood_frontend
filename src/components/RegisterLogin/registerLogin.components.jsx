7d976b1829430d647faf640341f91ed0170397d9
amirhallaji <a.hallaji.b@gmail.com>
2021-06-05 17:05:46 +0430

    started to set modal.

--- src/components/RegisterLogin/registerLogin.components.jsx
+++ src/components/RegisterLogin/registerLogin.components.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import './registerLogin.styles.css';
-import { Button } from 'antd';
+import { Button, Modal } from 'antd';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const showModal = () => {
+      setVisible(true);
+}
+
 
 export const RegisterLogin = () => {
+      const [visible, setVisible] = useState(false);
       return (
             <Link to="/register">
-                  <Button type="primary" shape="round" className='register-button'> ورود/عضویت </Button>
+                  <Button type="primary" shape="round" className='register-button' onClick={showModal}> ورود/عضویت </Button>
             </Link>
       )
 }
