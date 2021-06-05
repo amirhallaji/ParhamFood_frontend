b247b0d261be38aa89715c28a2aba57101ea0c1b
amirhallaji <a.hallaji.b@gmail.com>
2021-06-05 22:12:32 +0430

    components of the manager panel changed.

--- src/components/mpContainer/mpContainer.component.jsx
+++ src/components/mpContainer/mpContainer.component.jsx
@@ -1,17 +1,10 @@
 import React from 'react';
 import './mpContainer.styles.css';
 
-import { MpRightContainer } from '../mpRightContainer/mpRightContainer.component';
-import { MpLeftContainer } from '../mpLeftContainer/mpLeftContainer.component';
-import { MpCenterContainer } from '../mpCenterContainer/mpCenterContainer.component';
-import { MpHeader } from '../mpHeader/mpHeader.component';
 
 export const MpContainer = () => {
       return (
             <div className='mp-container'>
-                  <MpLeftContainer></MpLeftContainer>
-                  <MpCenterContainer></MpCenterContainer>
-                  <MpRightContainer></MpRightContainer>
             </div>
       )
 }
