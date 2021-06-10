15267fd1f8a1f7d9d3d0329452c6e9c55274450b
alireza mohammadi <ali9701mo@gmail.com>
2021-06-11 01:36:13 +0430

    some changes

--- src/components/userRegisterPageFields/userregisterPageFields.component.jsx
+++ src/components/userRegisterPageFields/userregisterPageFields.component.jsx
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './userRegisterPageFields.styles.css';
 import './../registerFields/registerFields.styles.css';
-import { Input, Button } from 'antd';
+import { Input, Button, message } from 'antd';
 import { Link } from 'react-router-dom';
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://127.0.0.1:4001";
 
 export const UserRegisterPageFields = () => {
+      const [response, setResponse] = useState("");
+      const [message, setMessage] = useState("");
+      const [messages, setMessages] = useState("");
+
+      useEffect(() => {
+            getMessages();
+      }, [message.length]);
+
+      const getMessages = () => {
+            const socket = socketIOClient(ENDPOINT);
+            socket.on("message", msg => {
+                  setMessages([...message, msg]);
+                  setMessage(msg)
+                  console.log(message);
+            });
+      };
+
+      useEffect(() => {
+      const socket = socketIOClient(ENDPOINT);
+      socket.on("test_client", data => {
+            setResponse(data);
+            console.log(response);
+      });
+      }, []);
+      
       return (
             <div className='total-register-fields'>
                   <div className='register-fields-container'>
                         <div className='register-fields-right'>
                               <div>
+                                    {/* <div className='register-type-fields'>نام و نام خانوادگی</div> */}
                                     <div className='register-type-fields'>نام و نام خانوادگی</div>
                                     <Input type="text" placeholder='اینجا وارد کنید'></Input>
                               </div>
@@ -45,7 +73,9 @@ export const UserRegisterPageFields = () => {
 
                   <div className='button-register-container'>
                         <Link to='/'>
-                              <Button className='button-register' type="primary" shape="round">ثبت نام</Button>
+                              <Button id="button_id_user" className='button-register' type="primary" shape="round" onclick={() => {
+                                    
+                              }}>ثبت نام</Button>
                         </Link>
                   </div>
             </div>
