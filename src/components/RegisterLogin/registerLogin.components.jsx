35971c964db503e151d4b97e5f991744363182f2
amirhallaji <a.hallaji.b@gmail.com>
2021-06-10 21:05:36 +0430

    basic modal added.

--- src/components/RegisterLogin/registerLogin.components.jsx
+++ src/components/RegisterLogin/registerLogin.components.jsx
@@ -1,19 +1,50 @@
 import React from 'react';
 import './registerLogin.styles.css';
-import { Button, Modal } from 'antd';
+import { Button, Modal, Input } from 'antd';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 
-const showModal = () => {
-}
 
 
 export const RegisterLogin = () => {
       const [visible, setVisible] = useState(false);
       return (
-            <Link to="/user-register">
-                  <Button type="primary" shape="round" className='register-button' onClick={showModal}> ورود/عضویت </Button>
-            </Link>
+            <div>
+                  <Button type="primary" shape="round" className='register-button' onClick={() => { setVisible(true) }}> ورود/عضویت </Button>
+                  <Modal className='modal' visible={visible} footer={null} onCancel={() => setVisible(false)}>
+
+                        <div className='modal-style'>
+                              <div style={{fontSize:'40px'}}>
+                                    پرهام فود
+                              </div>
+                              <div className='modal-style-field'>
+                                    <div style={{ direction: 'rtl', color: 'white' }}>شماره موبایل</div>
+                                    <Input type='number' placeholder='+989384158428'></Input>
+                              </div>
+                              <div className='modal-style-field'>
+                                    <div style={{ direction: 'rtl', color: 'white' }}>رمز عبور</div>
+                                    <Input type='password' placeholder='password'></Input>
+                              </div>
+                              <div className='modal-style-field'>
+                                    <div className='modal-buttons'>
+                                          <Button type='primary' shape='round'>ورود</Button>
+                                    </div>
+                              </div>
+                              <div className='modal-style-field'>
+                                    <div>.حساب کاربری ندارید؟ ثبت نام کنید</div>
+                              </div>
+                              <div className='modal-style-field'>
+                              <div className='modal-buttons'>
+                                          <Button type='primary' shape='round'>ثبت نام</Button>
+                                    </div>
+                              </div>
+                              
+
+                        </div>
+                  </Modal>
+            </div>
       )
+
+
 }
 
