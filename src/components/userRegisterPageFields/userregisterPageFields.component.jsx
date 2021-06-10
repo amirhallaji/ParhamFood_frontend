616374179c5cf23c0522f4dc61bd78ffb6470120
alireza mohammadi <ali9701mo@gmail.com>
2021-06-11 02:59:31 +0430

    some changes

--- src/components/userRegisterPageFields/userregisterPageFields.component.jsx
+++ src/components/userRegisterPageFields/userregisterPageFields.component.jsx
@@ -5,6 +5,7 @@ import { Input, Button, message } from 'antd';
 import { Link } from 'react-router-dom';
 import socketIOClient from "socket.io-client";
 const ENDPOINT = "http://127.0.0.1:4001";
+const socket = socketIOClient(ENDPOINT);
 
 export const UserRegisterPageFields = () => {
       const [response, setResponse] = useState("");
@@ -16,7 +17,6 @@ export const UserRegisterPageFields = () => {
       }, [message.length]);
 
       const getMessages = () => {
-            const socket = socketIOClient(ENDPOINT);
             socket.on("message", msg => {
                   setMessages([...message, msg]);
                   setMessage(msg)
@@ -25,12 +25,30 @@ export const UserRegisterPageFields = () => {
       };
 
       useEffect(() => {
-      const socket = socketIOClient(ENDPOINT);
       socket.on("test_client", data => {
             setResponse(data);
             console.log(response);
       });
       }, []);
+
+      let name;
+      let phone_number;
+      let password;
+      let confirm_password;
+      let region;
+      let address;
+
+      const submit_form = () => {
+            var json = {
+                  "phone_number": phone_number,   
+                  "password"    : password,
+                  "name"        : name,
+                  "region"      : region,
+                  "address"     : address
+            };
+            socket.emit('create user', json);
+            console.log(json);
+      }
       
       return (
             <div className='total-register-fields'>
@@ -39,33 +57,45 @@ export const UserRegisterPageFields = () => {
                               <div>
                                     {/* <div className='register-type-fields'>نام و نام خانوادگی</div> */}
                                     <div className='register-type-fields'>نام و نام خانوادگی</div>
-                                    <Input type="text" placeholder='اینجا وارد کنید'></Input>
+                                    <Input type="text" placeholder='اینجا وارد کنید' name="userName" onChange={(e) => {
+                                          name = e.target.value;
+                                    }}></Input>
                               </div>
 
                               <div>
                                     <div className='register-type-fields'>شماره موبایل</div>
-                                    <Input type="text" placeholder='+989123456789' style={{ direction: 'ltr' }}></Input>
+                                    <Input type="text" placeholder='+989123456789' name="userPhoneNumber" style={{ direction: 'ltr' }} onChange={(e) => {
+                                          phone_number = e.target.value;
+                                    }}></Input>
                               </div>
 
                               <div>
                                     <div className='register-type-fields'>رمز عبور(متشکل از اعداد و حروف و حداقل ۸ کاراکتر)</div>
-                                    <Input type="password" placeholder='password'></Input>
+                                    <Input type="password" placeholder='password' name="userPassword" onChange={(e) => {
+                                          password = e.target.value;
+                                    }}></Input>
                               </div>
                               <div>
                                     <div className='register-type-fields'>تأیید رمز عبور</div>
-                                    <Input type="password" placeholder='confirm password'></Input>
+                                    <Input type="password" placeholder='confirm password' name="userConfirmPassword" onChange={(e) => {
+                                          confirm_password = e.target.value;
+                                    }}></Input>
                               </div>
                         </div>
 
                         <div className='register-fields-left'>
                               <div>
                                     <div className='register-type-fields'>آدرس</div>
-                                    <Input type="text" placeholder='اینجا وارد کنید'></Input>
+                                    <Input type="text" placeholder='اینجا وارد کنید' name="userAddress" onChange={(e) => {
+                                          address = e.target.value;
+                                    }}></Input>
                               </div>
 
                               <div>
                                     <div className='register-type-fields'>منطقه</div>
-                                    <Input className='input-style' type="text" placeholder='اینجا وارد کنید'></Input>
+                                    <Input className='input-style' type="text" placeholder='اینجا وارد کنید' name="userRegion" onChange={(e) => {
+                                          region = e.target.value;
+                                    }}></Input>
                               </div>
                         </div>
 
@@ -73,9 +103,7 @@ export const UserRegisterPageFields = () => {
 
                   <div className='button-register-container'>
                         <Link to='/'>
-                              <Button id="button_id_user" className='button-register' type="primary" shape="round" onclick={() => {
-                                    
-                              }}>ثبت نام</Button>
+                              <Button id="button_id_user" className='button-register' type="primary" shape="round" onClick={submit_form()}>ثبت نام</Button>
                         </Link>
                   </div>
             </div>
