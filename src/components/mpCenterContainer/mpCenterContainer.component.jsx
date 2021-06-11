dc7f898d4217bc0a50e8cef568bb03fac2c24961
amirhallaji <a.hallaji.b@gmail.com>
2021-06-11 23:21:23 +0430

    initial table added to manager dashboard.

--- src/components/mpCenterContainer/mpCenterContainer.component.jsx
+++ src/components/mpCenterContainer/mpCenterContainer.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './mpCenterContainer.styles.css';
 
+import EditableTable from '../mpCenter/mpCenter.component';
+
 export const MpCenterContainer = () => {
       return (
             <div className='mp-center'>
                   {/* <MpCenter></MpCenter> */}
+                  <EditableTable></EditableTable>
             </div>
       )
 }
