4bfc1f47108408a852b690279bf8e085592625f1
amirhallaji <a.hallaji.b@gmail.com>
2021-06-13 10:43:57 +0430

    minor formatation

--- src/components/mpCenter/mpCenter.component.jsx
+++ src/components/mpCenter/mpCenter.component.jsx
@@ -1,14 +1,4 @@
-// import React from 'react';
-import './mpCenter.styles.css';// 
-// import { useContext, useState, useEffect, useRef } from 'react';
-// import { Table, Input, Button, Popconfirm, Form } from 'antd';
-// const EditableContext = React.createContext(null);
-
-// export const MpCenter = () => {
-      
-
-// }
-
+import './mpCenter.styles.css';
 import React, { useContext, useState, useEffect, useRef } from 'react';
 import { Table, Input, Button, Popconfirm, Form } from 'antd';
 const EditableContext = React.createContext(null);
