c7c6f533ed1cbf5d8870f1cb7c65e0ef93b1c0e1
amirhallaji <a.hallaji.b@gmail.com>
2021-06-23 06:58:49 +0430

    ourchase food page completed.

--- src/components/purchaseFoodFields/PurchaseFoodFields.jsx
+++ src/components/purchaseFoodFields/PurchaseFoodFields.jsx
@@ -3,17 +3,33 @@ import './PurchaseFoodFields.css';
 
 import pizza from '../../images/pizza.jpg'
 
+//antd
+import { Button } from 'antd';
+
 export const PurchaseFoodFields = () => {
       return (
             <div id='purchaseFoodContainer'>
                   <div className='purchaseFoodField'>
+
                         <div>
-                              نام غذا
+                              <img src={pizza} className='purchaseFoodFieldImage' />
                         </div>
-                        <div>
-                              نام رستوران
+
+                        <div className='purchaseFoodFieldName'>
+                              نام غذا: کباب کوبیده
                         </div>
+                        <div className='purchaseFoodFieldRestaurant'>
+                              نام رستوران: حلاج پلو
+                        </div>
+
+                        <div className='purchaseFoodFieldPrice'>
+                              قیمت غذا: 120000 تومان
+                        </div>
+
                   </div>
+                  <Button type='primary'>خرید نهایی</Button>
+
+
             </div>
       )
 }
